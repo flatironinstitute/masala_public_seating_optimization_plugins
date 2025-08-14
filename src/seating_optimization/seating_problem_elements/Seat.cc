8307ec90cffc9954daa74d12997b10f5ff3e60d1
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2025-08-14 16:08:39 -0700

    Intial commit of a seat.

---
+++ src/seating_optimization/seating_problem_elements/Seat.cc
@@ -0,0 +1,165 @@
+/*
+    Seating Optimization Masala Plugins 
+    Copyright (C) 2025 Vikram K. Mulligan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/// @file src/seating_optimization/seating_problem_elements/Seat.cc
+/// @brief Implementations for a Seat.
+/// @details A Seat is a place where a person can sit.  It's defined by a coordinate in R^2 and
+/// a direction (an angle, measured clockwise from north).
+/// @author Vikram K. Mulligan (vmulligan@flatironinstitute.org).
+
+// Unit header:
+#include <seating_optimization/seating_problem_elements/Seat.hh>
+
+// Numeric headers:
+
+// Base headers:
+#include <base/error/ErrorHandling.hh>
+#include <base/api/MasalaObjectAPIDefinition.hh>
+#include <base/api/constructor/MasalaObjectAPIConstructorMacros.hh>
+
+// STL headers:
+
+namespace seating_optimization_masala_plugins {
+namespace seating_optimization {
+namespace seating_problem_elements {
+
+////////////////////////////////////////////////////////////////////////////////
+// CONSTRUCTION AND DESTRUCTION
+////////////////////////////////////////////////////////////////////////////////
+
+/// @brief Copy constructor.  Explicit due to mutex.
+Seat::Seat( Seat const & src ) :
+    seating_optimization_masala_plugins::seating_optimization::seating_problem_elements::SeatingElementBase( src )
+{
+    std::lock< std::mutex, std::mutex >( mutex(), src.mutex() );
+    std::lock_guard< std::mutex > lockthis( mutex(), std::adopt_lock );
+    std::lock_guard< std::mutex > lockthat( src.mutex(), std::adopt_lock );
+    Seat::assign( src );
+}
+
+
+/// @brief Make a copy of this object.
+seating_optimization_masala_plugins::seating_optimization::seating_problem_elements::SeatingElementBaseSP
+Seat::clone() const {
+    return masala::make_shared< Seat >( *this );
+}
+
+/// @brief Make a fully independent copy of this object.
+SeatSP
+Seat::deep_clone() const {
+    SeatSP new_seat( std::static_pointer_cast< Seat >( this->clone() ) );
+    new_seat->make_independent();
+    return new_seat;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// PUBLIC MEMBER FUNCTIONS
+////////////////////////////////////////////////////////////////////////////////
+
+/// @brief Get the category or categories for this plugin class.  Default for all
+/// optimization problems; may be overridden by derived classes.
+/// @returns { { "OptimizationSolution", "CostFunctionNetworkOptimizationSolution", "Seat" } }
+/// @note Categories are hierarchical (e.g. Selector->AtomSelector->AnnotatedRegionSelector,
+/// stored as { {"Selector", "AtomSelector", "AnnotatedRegionSelector"} }). A plugin can be
+/// in more than one hierarchical category (in which case there would be more than one
+/// entry in the outer vector), but must be in at least one.  The first one is used as
+/// the primary key.
+std::vector< std::vector< std::string > >
+Seat::get_categories() const {
+	return std::vector< std::vector< std::string > > {
+		{ "SeatingProblem", "SeatingProblemElement", "Seat" }
+	};
+}
+
+/// @brief Get the keywords for this plugin class.  Default for all
+/// optimization solutions; may be overridden by derived classes.
+/// @returns { "optimization_solution", "qubo_solution", "numeric" }
+std::vector< std::string >
+Seat::get_keywords() const {
+	return std::vector< std::string > {
+        "seating_problem",
+		"seating_problem_element",
+        "seat",
+	};
+}
+
+/// @brief Get the name of this class.
+/// @returns "Seat".
+std::string
+Seat::class_name() const {
+    return "Seat";
+}
+
+/// @brief Get the namespace for this class.
+/// @returns "masala::numeric::optimization::cost_function_network".
+std::string
+Seat::class_namespace() const {
+    return "seating_optimization_masala_plugins::seating_optimization::seating_problem_elements";
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// PUBLIC INTERFACE DEFINITION
+////////////////////////////////////////////////////////////////////////////////
+
+/// @brief Get a description of the API for the Seat class.
+masala::base::api::MasalaObjectAPIDefinitionCWP
+Seat::get_api_definition() {
+    using namespace masala::base::api;
+    using masala::base::Real;
+    using masala::base::Size;
+
+    std::lock_guard< std::mutex > lock( solution_mutex() );
+
+    if( api_definition() == nullptr ) {
+
+        MasalaObjectAPIDefinitionSP api_def(
+            masala::make_shared< MasalaObjectAPIDefinition >(
+                *this,
+                "The Seat class stores all information associated with a seat at which a guest could be seated.",
+                false, false
+            )
+        );
+        ADD_PUBLIC_CONSTRUCTOR_DEFINITIONS( Seat, api_def );
+
+        // Work functions:
+
+        // Getters:
+
+        // Setters:
+
+        api_definition() = api_def; //Make const.
+    }
+
+    return api_definition();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// PUBLIC GETTERS
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+// PUBLIC SETTERS
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+// PUBLIC WORK FUNCTIONS
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace seating_problem_elements
+} // namespace seating_optimization
+} // namespace seating_optimization_masala_plugins
---
+++ src/seating_optimization/seating_problem_elements/Seat.hh
@@ -0,0 +1,146 @@
+/*
+    Seating Optimization Masala Plugins 
+    Copyright (C) 2025 Vikram K. Mulligan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/// @file src/seating_optimization/seating_problem_elements/Seat.hh
+/// @brief Headers for a Seat.
+/// @details A Seat is a place where a person can sit.  It's defined by a coordinate in R^2 and
+/// a direction (an angle, measured clockwise from north).
+/// @author Vikram K. Mulligan (vmulligan@flatironinstitute.org).
+
+#ifndef Seating_Optimization_Masala_Plugins_src_seating_optimization_seating_problem_elements_Seat_hh
+#define Seating_Optimization_Masala_Plugins_src_seating_optimization_seating_problem_elements_Seat_hh
+
+// Forward declarations:
+#include <seating_optimization/seating_problem_elements/Seat.fwd.hh>
+
+// Parent header:
+#include <seating_optimization/seating_problem_elements/SeatingElementBase.hh>
+
+// Base headers:
+#include <base/types.hh>
+
+namespace seating_optimization_masala_plugins {
+namespace seating_optimization {
+namespace seating_problem_elements {
+
+/// @brief A Seat.
+/// @details A Seat is a place where a person can sit.  It's defined by a coordinate in R^2 and
+/// a direction (an angle, measured clockwise from north).
+/// @author Vikram K. Mulligan (vmulligan@flatironinstitute.org).
+class Seat : public seating_optimization_masala_plugins::seating_optimization::seating_problem_elements::SeatingElementBase {
+
+public:
+
+////////////////////////////////////////////////////////////////////////////////
+// CONSTRUCTION AND DESTRUCTION
+////////////////////////////////////////////////////////////////////////////////
+
+	/// @brief Default constructor.
+	Seat() = default;
+
+	/// @brief Copy constructor.  Explicit due to mutex.
+	Seat( Seat const & src );
+
+	/// @brief Destructor.
+	~Seat() override = default;
+
+	/// @brief Make a copy of this object.
+	seating_optimization_masala_plugins::seating_optimization::seating_problem_elements::SeatingElementBaseSP
+	clone() const override;
+
+	/// @brief Make a fully independent copy of this object.
+	SeatSP
+	deep_clone() const;
+
+public:
+
+////////////////////////////////////////////////////////////////////////////////
+// PUBLIC MEMBER FUNCTIONS
+////////////////////////////////////////////////////////////////////////////////
+
+	/// @brief Get the category or categories for this plugin class.  Default for all
+	/// optimization problems; may be overridden by derived classes.
+	/// @returns { { "OptimizationSolution", "CostFunctionNetworkOptimizationSolution", "Seat" } }
+	/// @note Categories are hierarchical (e.g. Selector->AtomSelector->AnnotatedRegionSelector,
+	/// stored as { {"Selector", "AtomSelector", "AnnotatedRegionSelector"} }). A plugin can be
+	/// in more than one hierarchical category (in which case there would be more than one
+	/// entry in the outer vector), but must be in at least one.  The first one is used as
+	/// the primary key.
+	std::vector< std::vector< std::string > >
+	get_categories() const override;
+
+	/// @brief Get the keywords for this plugin class.  Default for all
+	/// optimization solutions; may be overridden by derived classes.
+	/// @returns { "optimization_solution", "cost_function_network_optimization_solution", "qubo_solution", "numeric" }
+	std::vector< std::string >
+	get_keywords() const override;
+
+	/// @brief Get the name of this class.
+	/// @returns "Seat".
+	std::string
+	class_name() const override;
+
+	/// @brief Get the namespace for this class.
+	/// @returns "seating_optimization_masala_plugins::seating_optimization::seating_problem_elements".
+	std::string
+	class_namespace() const override;
+
+public:
+
+////////////////////////////////////////////////////////////////////////////////
+// PUBLIC INTERFACE DEFINITION
+////////////////////////////////////////////////////////////////////////////////
+
+    /// @brief Get a description of the API for the CostFunctionNetworkOptimizationSolution class.
+    masala::base::api::MasalaObjectAPIDefinitionCWP
+    get_api_definition() override;
+
+public:
+
+////////////////////////////////////////////////////////////////////////////////
+// PUBLIC GETTERS
+////////////////////////////////////////////////////////////////////////////////
+
+
+public:
+
+////////////////////////////////////////////////////////////////////////////////
+// PUBLIC SETTERS
+////////////////////////////////////////////////////////////////////////////////
+
+public:
+
+////////////////////////////////////////////////////////////////////////////////
+// PUBLIC WORK FUNCTIONS
+////////////////////////////////////////////////////////////////////////////////
+
+
+private:
+
+////////////////////////////////////////////////////////////////////////////////
+// PRIVATE DATA
+////////////////////////////////////////////////////////////////////////////////
+
+
+}; // class Seat
+
+} // namespace seating_problem_elements
+} // namespace seating_optimization
+} // namespace seating_optimization_masala_plugins
+
+#endif // Seating_Optimization_Masala_Plugins_src_seating_optimization_seating_problem_elements_Seat_hh

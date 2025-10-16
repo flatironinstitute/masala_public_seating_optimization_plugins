4728dc1ea8c19e39b9c1a2b8e77ee312ef776875
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2025-10-16 13:48:06 -0400

    Initial commit of visualization script.

---
+++ python/visualize_solutions.py
@@ -0,0 +1,22 @@
+################################################################################
+##    Seating Optimization Masala Pugins
+##    Copyright (C) 2025 Vikram K. Mulligan
+##
+##    This program is free software: you can redistribute it and/or modify
+##    it under the terms of the GNU Affero General Public License as published by
+##    the Free Software Foundation, either version 3 of the License, or
+##    (at your option) any later version.
+##
+##    This program is distributed in the hope that it will be useful,
+##    but WITHOUT ANY WARRANTY; without even the implied warranty of
+##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##    GNU Affero General Public License for more details.
+##
+##    You should have received a copy of the GNU Affero General Public License
+##    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+################################################################################
+
+## @file python/visualize_solutions.py
+## @brief Draws seating diagrams from optimization problem descriptions and solution files.
+## @author Vikram K. Mulligan
+

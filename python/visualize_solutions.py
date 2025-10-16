84f0175251e345fbbc861019a2081e672eeb31d2
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2025-10-16 18:17:05 -0400

    OK, drawing room properly now.

--- python/visualize_solutions.py
+++ python/visualize_solutions.py
@@ -27,7 +27,7 @@
 
 import argparse
 import drawsvg as draw
-from math import sqrt
+from math import sqrt, sin, cos
 
 ################################################################################
 ## FUNCTION DEFINITIONS
@@ -53,13 +53,94 @@ def read_file( filename : str )->list[str] :
     #print( outlist )
     return outlist
 
+## @brief Rotate a point about another.
+def rotate( p : tuple[float,float], xcen : float, ycen : float, angle : float) -> tuple[float,float] :
+    x = p[0]-xcen
+    y = p[1]-ycen
+    xprime = x*cos(angle) - y*sin(angle)
+    xprime = x*sin(angle) + y*cos(angle)
+    return (x,y)
+
+## @brief Given a rotated rectangle, get its minimum and maximum coordinates.
+def get_bounds_of_rotated_rectangle( x : float, y : float, width : float, length : float, angle : float )->tuple[float,float,float,float] :
+    halfwidth = width/2
+    halflength = length/2
+    p1 = (x-halfwidth,y-halflength)
+    p2 = (x+halfwidth,y-halflength)
+    p3 = (x-halfwidth,y+halflength)
+    p4 = (x+halfwidth,y+halflength)
+
+    p1 = rotate(p1, x, y, angle)
+    p2 = rotate(p2, x, y, angle)
+    p3 = rotate(p3, x, y, angle)
+    p4 = rotate(p4, x, y, angle)
+
+    print(p1,p2,p3,p4)
+
+    return min( [ p1[0], p2[0], p3[0], p4[0] ] ), max( [ p1[0], p2[0], p3[0], p4[0] ] ), min( [ p1[1], p2[1], p3[1], p4[1] ] ), max( [ p1[1], p2[1], p3[1], p4[1] ] )
+
+
+## @brief Read the rooms from the problem file and return a vector of rooms, plus the bounds of the drawing.
+def generate_rooms( problines : list[str] )->tuple[ list[draw.DrawingBasicElement], float, float, float, float ] :
+    inrooms = False
+    minx = None
+    maxx = None
+    miny = None
+    maxy = None
+    outvec = []
+    
+    for fullline in problines:
+        line = fullline.strip()
+        if inrooms == False :
+            if line == "ROOMS:" :
+                inrooms = True
+        else :
+            if line == "" :
+                inrooms = False
+                break
+            linesplit = line.split()
+            assert len(linesplit) > 2
+            if linesplit[0] == "Room_index" :
+                continue # Header line
+            if linesplit[1] == "RectangularRoom" :
+                assert len(linesplit) == 9
+                x = float(linesplit[2])
+                y = float(linesplit[3])
+                angle = float(linesplit[4])
+                length = float(linesplit[6])
+                width = float(linesplit[8])
+                r1 = draw.Rectangle( x-width/2, y-length/2, width, length, stroke_width=0.025, stroke="black", fill="#a4badb" )
+                g1 = draw.Group( [r1], transform="rotate(" + str(-angle) + "," + str(x) + "," + str(y) + ")" )
+                boundx_low, boundx_high, boundy_low, boundy_high = get_bounds_of_rotated_rectangle( x, y, width, length, angle )
+                if minx == None or boundx_low < minx :
+                    minx = boundx_low
+                if maxx == None or boundx_high > maxx :
+                    maxx = boundx_high
+                if miny == None or boundy_low < miny :
+                    miny = boundy_low
+                if maxy == None or boundy_high > maxy :
+                    maxy = boundy_high
+                outvec.append(g1)
+                print( "Parsed RectangularRoom at " + str(x) + ", " + str(y) + "." )
+            else :
+                raise Exception( "Did not recognize room type \"" + linesplit[1] + "\"." )
+    
+    xwidth = maxx - minx
+    ywidth = maxy - miny
+    xcen = minx + xwidth/2
+    ycen = miny + ywidth/2
+    print( xwidth, ywidth, xcen, ycen )
+    return outvec, xwidth, ywidth, xcen, ycen
+
+        
+
 ## @brief Draw the tables.  Return a vector of table centre coordinates.
 def draw_tables( drawing : draw.Drawing, problines : list[str] )->list[tuple[float,float]] :
     intables = False
     table_coords = []
     for fullline in problines :
         line = fullline.strip()
-        print(line)
+        #print(line)
         if intables == False :
             if line == "TABLES:" :
                 intables = True
@@ -73,7 +154,7 @@ def draw_tables( drawing : draw.Drawing, problines : list[str] )->list[tuple[flo
                 continue # Header line
             if linesplit[1] == "CircularTable" :
                 assert len(linesplit) == 7
-                tableindex = int(linesplit[0])
+                #tableindex = int(linesplit[0])
                 x = float(linesplit[2])
                 y = float(linesplit[3])
                 #angle = float(linesplit[4])
@@ -154,7 +235,11 @@ print( "solution_filenames =", solution_filenames )
 problines = read_file(problem_filename)
 
 for i in range(len(solution_filenames)) :
-    drawing = draw.Drawing( 12, 8, origin="center" )
+    rooms, drawingwidth, drawinglength, drawingorigin_x, drawingorigin_y = generate_rooms( problines )
+    print (drawingorigin_x, drawingorigin_y)
+    drawing = draw.Drawing( drawingwidth+.5, drawinglength+.5, origin=( drawingorigin_x - (drawingwidth+.5)/2, drawingorigin_y - (drawinglength + .5)/2 ) )
+    for room in rooms:
+        drawing.append(room)
     table_coords = draw_tables( drawing, problines )
     seat_coords = draw_seats( drawing, problines )
     soln_lins = read_file(solution_filenames[i])

f80bad006953b6947fbc966931212d092e3f067d
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2026-03-22 19:18:57 -0400

    *Sigh*.  Rosetta's one-hot convention will be the death of me.

--- src/seating_optimization/seating_problem/SeatingProblem.cc
+++ src/seating_optimization/seating_problem/SeatingProblem.cc
@@ -1034,7 +1034,7 @@ SeatingProblem::prime_choices(
 		"type " + problem.class_name() + " as a PluginPairwisePrecomputedCostFunctionNetworkOptimizationProblem."
 	);
 
-	for( Size iguest(0); iguest <= guest_choice_to_seat_index.size(); ++iguest ){
+	for( Size iguest(0); iguest < guest_choice_to_seat_index.size(); ++iguest ){
 		problem.set_minimum_number_of_choices_at_node( iguest, guest_choice_to_seat_index[iguest].size() );
 		for( auto const & entry : guest_choice_to_seat_index[iguest] ) {
 			problem_cast->set_onebody_penalty( iguest, entry.first, 0 );
